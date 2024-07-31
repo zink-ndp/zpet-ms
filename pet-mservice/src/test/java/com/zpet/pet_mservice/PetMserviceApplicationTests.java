1685820f4db3e5aae25a285db00bb83003631486
zink <phucvinhlp02@gmail.com>
2024-07-31 20:46:33 +0700

    first commit

---
+++ pet-mservice/src/test/java/com/zpet/pet_mservice/PetMserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zpet.pet_mservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetMserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
