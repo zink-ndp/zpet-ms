69f99eb10b2901e5e33c071a913919f1e8b8bcf3
zink-ndp <phucvinhlp02@gmail.com>
2024-08-08 17:51:36 +0700

    created basic staff APIs

--- ms-staff/src/main/java/com/zpet/ms_staff/repository/StaffRepository.java
+++ ms-staff/src/main/java/com/zpet/ms_staff/repository/StaffRepository.java
@@ -1,13 +1,30 @@
 package com.zpet.ms_staff.repository;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.zpet.ms_staff.mapper.StaffMapper;
+import com.zpet.ms_staff.model.Staff;
 
 @Repository
 public class StaffRepository {
-    
-    @Autowired StaffMapper staffMapper;
+
+    @Autowired
+    StaffMapper staffMapper;
+
+    public List<Staff> getAll(Map<String, Object> params) {
+        return staffMapper.getAll(params);
+    }
+
+    public Staff getById(Map<String, Object> params){
+        return staffMapper.getById(params);
+    }
+
+    public Staff getByPhone(Map<String, Object> params){
+        return staffMapper.getByPhone(params);
+    }
 
 }
--- ms-staff/src/main/java/com/zpet/ms_staff/service/StaffService.java
+++ ms-staff/src/main/java/com/zpet/ms_staff/service/StaffService.java
@@ -1,13 +1,40 @@
 package com.zpet.ms_staff.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.zpet.ms_staff.model.Staff;
 import com.zpet.ms_staff.repository.StaffRepository;
+import com.zpet.ms_staff.util.FunctionUtils;
 
 @Service
 public class StaffService {
     
     @Autowired StaffRepository staffRepository;
+
+    @Autowired FunctionUtils functionUtils;
+
+    public List<Staff> getAll(Map<String, Object> params) {
+        List<Staff> staffs = staffRepository.getAll(params);
+        staffs.forEach(s -> {
+            s.setJoinedDate(functionUtils.formatDate(s.getJoinedDate(), "dd/MM/yyyy"));
+        });
+        return staffs;
+    }
+
+    public Staff getById(Map<String, Object> params){
+        Staff s = staffRepository.getById(params);
+        s.setJoinedDate(functionUtils.formatDate(s.getJoinedDate(), "dd/MM/yyyy"));
+        return s;
+    }
+
+    public Staff getByPhone(Map<String, Object> params){
+        Staff s = staffRepository.getByPhone(params);
+        s.setJoinedDate(functionUtils.formatDate(s.getJoinedDate(), "dd/MM/yyyy"));
+        return s;
+    }
 
 }
