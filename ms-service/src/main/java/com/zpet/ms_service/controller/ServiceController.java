4f496e320aa67327f911a3da257110a8cbc3d6d2
zink-ndp <phucvinhlp02@gmail.com>
2024-08-10 14:40:07 +0700

    created basic service APIs

--- ms-service/src/main/java/com/zpet/ms_service/controller/ServiceController.java
+++ ms-service/src/main/java/com/zpet/ms_service/controller/ServiceController.java
@@ -1,13 +1,63 @@
 package com.zpet.ms_service.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.zpet.ms_service.model.Service;
 import com.zpet.ms_service.service.ServiceService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController
 public class ServiceController {
     
     @Autowired ServiceService serviceService;
+
+    @GetMapping("/all")
+    public List<Service> getAll(
+        @RequestParam(required = false) String name,
+        @RequestParam(required = false) Double fromPrice,
+        @RequestParam(required = false) Double toPrice,
+        @RequestParam(required = false) Integer isAvailable
+    ) {
+        Map<String, Object> params = new HashMap<>();
+        if (name != null) params.put("name", "%"+name+"%");
+        if (isAvailable != null) params.put("isAvailable", isAvailable);
+        if (fromPrice != null && toPrice != null) {
+            params.put("fromPrice", fromPrice);
+            params.put("toPrice", toPrice);
+        } 
+        return serviceService.getAll(params);
+    } 
+
+    @PostMapping("/create")
+    public ResponseEntity<Object> createService(@RequestBody Service service) {
+        try {
+            serviceService.create(service);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<Object> updateService(@RequestBody Service service) {
+        try {
+            serviceService.update(service);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    
 
 }
--- ms-service/src/main/java/com/zpet/ms_service/service/ServiceService.java
+++ ms-service/src/main/java/com/zpet/ms_service/service/ServiceService.java
@@ -1,13 +1,34 @@
 package com.zpet.ms_service.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.zpet.ms_service.model.Service;
 import com.zpet.ms_service.repository.ServiceRepository;
 
-@Service
+@org.springframework.stereotype.Service
 public class ServiceService {
-    
-    @Autowired ServiceRepository serviceRepository;
+
+    @Autowired
+    ServiceRepository serviceRepository;
+
+    public List<Service> getAll(Map<String, Object> params) {
+        return serviceRepository.getAll(params);
+    }
+
+    @Transactional
+    public void create(Service service){
+        Integer nextId = serviceRepository.lastId() + 1;
+        service.setId(nextId);
+        serviceRepository.create(service);
+    }
+
+    @Transactional
+    public void update(Service service){
+        serviceRepository.create(service);
+    }
 
 }
